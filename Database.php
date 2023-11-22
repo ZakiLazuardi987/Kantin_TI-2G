149fc67cb997c20cb915288ed2aed90e4191304e
WulanMPF <wulanmaulidya1604@gmail.com>
2023-11-22 19:51:02 +0700

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,15 +1,15 @@
 <?php
-class Database{
+class Database {
     private $host = "localhost";
     private $username = "root";
     private $password = "";
     private $database = "dbproject";
     public $conn;
 
-    public function __construct(){
+    public function __construct() {
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
 
-        if($this->conn->connect_error){
+        if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
     }
