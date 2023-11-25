942f55846cab588daa137b8854d5389c44168aba
WulanMPF <wulanmaulidya1604@gmail.com>
2023-11-25 10:09:57 +0700

    Update Database.php

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@ class Database {
     private $host = "localhost";
     private $username = "root";
     private $password = "";
-    private $database = "dbproject";
+    private $database = "dbpercobaan";
     public $conn;
 
     public function __construct() {
