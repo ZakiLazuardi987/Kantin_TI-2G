dc1aefd81218abc9c94a99411c7a421cdfee5810
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2023-11-26 13:08:40 +0700

    Create inbox_pesan.php

---
+++ Notifications/inbox_pesan.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include "koneksi.php";
+
+if ($_SESSION['level'] == 'admin') {
+    // Jika login sebagai admin, tampilkan pengajuan
+    $sql = "SELECT * FROM pengajuan ORDER BY tanggal DESC";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Level</th>
+                    <th>Alasan</th>
+                    <th>Tanggal</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                    <td>{$row['id']}</td>
+                    <td>{$row['username']}</td>
+                    <td>{$row['level']}</td>
+                    <td>{$row['alasan']}</td>
+                    <td>{$row['tanggal']}</td>
+                    <td>{$row['status']}</td>
+                    <td><a href='proses_inbox.php?id={$row['id']}&action=setuju'>Setuju</a> | <a href='proses_inbox.php?id={$row['id']}&action=tidak_setuju'>Tidak Setuju</a></td>
+                  </tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Tidak ada pengajuan.";
+    }
+} else {
+    // Jika login sebagai pegawai, beri tahu bahwa halaman ini hanya untuk admin
+    echo "Halaman ini hanya dapat diakses oleh admin.";
+}
+
+$conn->close();
+?>
