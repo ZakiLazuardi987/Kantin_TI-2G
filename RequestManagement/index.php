1041226980f18f4ef1cfcdd92c3d5bfcb3717689
Zaki Lazuardi <zakilfp987@gmail.com>
2023-11-27 10:04:09 +0700

    Update index.php
    index menampilkan menurequest.php

--- RequestManagement/index.php
+++ RequestManagement/index.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Request Management</title>
-</head>
-<body>
-    <h1>Selamat datang di Sistem Manajemen Permintaan</h1>
-    <ul>
-        <li><a href="MenuRequest.php">Kelola Permintaan Menu</a></li>
-        <li><a href="RequestApproval.php">Proses Persetujuan atau Penolakan Permintaan</a></li>
-    </ul>
-</body>
-</html>
+<?php
+// File: index.php
+
+header("Location: MenuRequest.php");
+exit();
+?>
