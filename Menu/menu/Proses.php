1b943ff28030090ef8372b485ad5e89b2483ce0a
cindyvira03 <cindyvira929@gmail.com>
2023-11-29 08:22:50 +0700

    Create Proses.php

---
+++ Menu/menu/Proses.php
@@ -0,0 +1,99 @@
+<?php
+session_start();
+include_once '../Database.php';
+include_once 'Menu.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$menu = new Menu($db);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['submit'])) {
+            $id_kategori = $_POST['kategori'];
+            $nama_produk = $_POST['nama_produk'];
+            $harga = $_POST['harga'];
+            $stok = $_POST['stok'];
+            $gambar_produk = $_FILES['gambar_produk']['name'];
+            $nama_gambar_baru = ''; // Variabel untuk menyimpan nama gambar baru
+            if (!empty($_FILES['gambar_produk']['name'])) {
+                $gambar_produk = $_FILES['gambar_produk'];
+                $nama_gambar_baru = generateRandomName($gambar_produk['name']); // Mendapatkan nama acak untuk gambar baru
+
+                $uploadDir = 'gambar_produk/';
+                $uploadFile = $uploadDir . $nama_gambar_baru;
+
+                if (move_uploaded_file($gambar_produk['tmp_name'], $uploadFile)) {
+                    // Jika upload berhasil, lanjutkan dengan operasi di database
+                    if ($menu->addProduct($id_kategori, $nama_produk, $harga, $stok, $nama_gambar_baru)) {
+                        
+                        $_SESSION['success_message'] = "Menu berhasil ditambahkan!";
+                        
+                    } else {
+                        $_SESSION['error_message'] = "Gagal menambahkan menu.";
+                    }
+                } else {
+                    $_SESSION['error_message'] = "Gagal mengunggah gambar.";
+                }
+            } else {
+            $_SESSION['error_message'] = "Harap pilih gambar.";
+            }
+            header("Location: index.php");
+            exit();
+
+        } else if(isset($_POST['update'])){
+            $id_produk = $_POST['id_produk'];
+            $id_kategori = $_POST['kategori'];
+            $nama_produk = $_POST['nama_produk'];
+            $harga = $_POST['harga'];
+            $stok = $_POST['stok'];
+            $gambar_produk = $_FILES['gambar_produk']['name'];
+            $nama_gambar_baru = ''; // Variabel untuk menyimpan nama gambar baru
+            if (!empty($_FILES['gambar_produk']['name'])) {
+                $gambar_produk = $_FILES['gambar_produk'];
+                $nama_gambar_baru = generateRandomName($gambar_produk['name']); // Mendapatkan nama acak untuk gambar baru
+
+                $uploadDir = 'gambar_produk/';
+                $uploadFile = $uploadDir . $nama_gambar_baru;
+
+                if($menu->updateProduct($id_produk, $id_kategori, $nama_produk, $harga, $stok, $nama_gambar_baru)){
+                    $_SESSION['success_message'] = "Menu berhasil diedit!";
+                } else {
+                    $_SESSION['error_message'] = "Gagal mengedit menu.";
+                }
+            } else{
+                if($menu->updateNoPict($id_produk, $id_kategori, $nama_produk, $harga, $stok)){
+                    $_SESSION['success_message'] = "Menu berhasil diedit!";
+                } else {
+                    $_SESSION['error_message'] = "Gagal mengedit menu.";
+                }
+            }
+
+            
+
+            header("Location: index.php");
+            exit();
+        }
+
+        
+    }
+
+    if(isset($_GET['action']) && $_GET['action'] === 'delete'){
+        $id_produk = $_GET['id_produk'];
+        
+        if($menu->deleteProduct($id_produk)){
+            $_SESSION['success_message'] = "Menu berhasil dihapus!";
+        } else {
+            $_SESSION['message'] = "Gagal menghapus menu.";
+        }
+
+        header("Location: index.php");
+        exit();
+    }
+
+    function generateRandomName($fileName)
+{
+    $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+    return uniqid() . '.' . $ext; // Menghasilkan nama acak untuk gambar baru
+}
+?>
