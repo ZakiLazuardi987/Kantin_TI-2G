ea949905d84a57fabaf3ebd5cdc046ba1bca63da
cindyvira03 <cindyvira929@gmail.com>
2023-12-04 09:16:59 +0700

    Create navbar.php

---
+++ app/views/admin/template/navbar.php
@@ -0,0 +1,29 @@
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand" style="background-color: #e3f2fd;">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars" style="color: #3D72AA"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+      <a class="navbar-brand" href="#" style="color: #3D72AA">
+        <img src="<?= BASEURL?>/assets/brand/polinema.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+            Kantin JTI Polinema
+          </a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <a class="navbar-brand" href="#" style="color: #3D72AA">
+        <i class="nav-icon fas fa-user"></i>
+                Hello Admin!
+            </a>
+        </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
