6d2fee6f4a4e9c2f8c15d49679a8c6a0940b0c81
cindyvira03 <cindyvira929@gmail.com>
2023-12-04 09:15:44 +0700

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url = explode("/", $url);
+
+$baseurl = 'http://' . $url[2] . '/mvc-php';
+
+define('BASEURL', $baseurl);
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'dbpercobaan');
+
+$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
