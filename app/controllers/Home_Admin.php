d92350e3b9b70b26c7873bd7e9862acee34864e5
cindyvira03 <cindyvira929@gmail.com>
2023-12-04 09:15:51 +0700

    Create Home_Admin.php

---
+++ app/controllers/Home_Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+class Home_Admin extends Controller {
+    public function index()
+    {
+        $data['title'] = 'Home Admin';
+        $data['data'] = $this->model('Produk_Model')->getAllProducts();
+
+        $this->view('admin/template/header', $data);
+        $this->view('admin/template/navbar');
+        $this->view('admin/template/sidebar');
+        $this->view('admin/home/index', $data);
+        $this->view('admin/template/footer');
+    }
+
+}
