b7733f8433e076dbfbd14adb2c06f19bdd81d6df
cindyvira03 <cindyvira929@gmail.com>
2023-12-05 11:12:12 +0700

    Update index.php

--- app/views/admin/home/index.php
+++ app/views/admin/home/index.php
@@ -1,60 +1,51 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <?php if ($data['title'] !== 'Menu Produk') : ?>
-                        <h1 class="m-0">Menu Produk</h1>
-                    <?php endif; ?>
-                </div><!-- /.col -->
-                <div class="col-sm-6 d-flex justify-content-end align-items-center">
-                    <ol class="breadcrumb ml-3">
-                        <li class="breadcrumb-item active">Admin</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
+    
+
+
+    <!-- /.content-header -->
 
     <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <?php if ($data['title'] === 'Menu Produk') : ?>
-                <div class="row">
-                    <?php foreach ($data['data'] as $produk) : ?>
-                        <div class="card card-spacing">
-                            <!-- ... (rest of your card content) -->
+    
+      <div class="container-fluid">
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    <div class=
+                    "d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+                    <h1 class="h2">Dashboard</h1>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div id="card-dashboard" class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title"><strong>PRODUK</strong></h5>
+                                    <!-- <p class="card-text">Total anggota sejumlah <?= $row_anggota['jml'] ?> orang.</p> -->
+                                    
+
+                                </div>
+
+                            </div>
+
                         </div>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif; ?>
-        </div>
+                        <div class="col-sm-6">
+                            <div id="card-dashboard" class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title"><strong>PENJUALAN</strong></h5>
+                                    <!-- <p class="card-text">Total jabatan sejumlah <?= $row_jabatan['jml'] ?> orang.</p> -->
+                                    
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    
+                    </div>
+                </main>
+
+        
     </div>
-    <!-- /.content -->
 </div>
-<!-- /.content-wrapper -->
+  <!-- /.content-wrapper -->
 <!-- Modal -->
-<
-
-<script>
-  function ubahdata(x){
-    $('.modal-title').html('Ubah Data Produk');
-    let url = '<?=BASEURL?>/Produk_Admin/formUbah';
-    $.post(url,
-    {
-      id_produk : x
-    }, function(data, success){
-      $('.modal-body').html(data);
-    });
-  }
-
-  function hapusdata(x){
-    $('.modal-title').html('Hapus Data Produk');
-    $('.modal-body').html('Apakah Anda Yakin Akan Menghapus Data ini?');
-    $('.tombol').html('<a href="<?= BASEURL?>/Produk_Admin/prosesHapus/' + x + '" class="btn btn-secondary" style="background: #A52222">Hapus</a>');
-    $('#close').html('Batal');
-  } 
-</script>
-
