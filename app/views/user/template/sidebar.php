47746989308b8c7b46ec5733161df972f5a985c4
cindyvira03 <cindyvira929@gmail.com>
2023-12-05 11:12:37 +0700

    Create sidebar.php

---
+++ app/views/user/template/sidebar.php
@@ -0,0 +1,65 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-light-info elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="<?= BASEURL?>/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">User Kantin</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <a class="navbar-brand ml-3" href="#">
+        <i class="nav-icon fas fa-user"></i>
+                Nama Pegawai
+            </a>
+    
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item mb-3">
+            <a href="<?= BASEURL?>/Produk_User" class="nav-link active">
+              <i class="nav-icon fas fa-box"></i>
+              <p>
+                Stok
+                <!-- <i class="right fas fa-angle-left"></i> -->
+              </p>
+            </a>
+          </li>
+          <li class="nav-item mb-3">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-user-tie"></i>
+              <p>
+                Pengajuan
+                <!-- <i class="right fas fa-angle-left"></i> -->
+              </p>
+            </a>
+          </li>
+          <li class="nav-item mb-3">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-file-alt"></i>
+              <p>
+                History Penjualan
+                <!-- <i class="right fas fa-angle-left"></i> -->
+              </p>
+            </a>
+          </li>
+          <li class="nav-item mb-3">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>
+                Log Out
+                <!-- <i class="right fas fa-angle-left"></i> -->
+              </p>
+            </a>
+          </li>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
