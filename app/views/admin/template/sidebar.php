6d30228a69b2a2dc645cea6a177ddb861424b202
cindyvira03 <cindyvira929@gmail.com>
2023-12-07 09:19:33 +0700

    Update sidebar.php

--- app/views/admin/template/sidebar.php
+++ app/views/admin/template/sidebar.php
@@ -1,5 +1,5 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-light-info elevation-4">
+<aside class="main-sidebar sidebar-light-info elevation-4" style="position: fixed;">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="<?= BASEURL?>/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
