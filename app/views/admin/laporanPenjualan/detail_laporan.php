7455de6bc6c5ded61cbf1513b2e611505b416c3a
WulanMPF <wulanmaulidya1604@gmail.com>
2023-12-07 20:09:05 +0700

    Create detail_laporan.php

---
+++ app/views/admin/laporanPenjualan/detail_laporan.php
@@ -0,0 +1,96 @@
+<div class="content">
+  <div class="container-fluid">
+    <div class="modal-body">    
+    <div class="row">
+      <!-- <div class="modal-body"> -->
+        <!-- <div class="col-md-6"> -->
+        <div class="card mx-auto" style="width: 400px; height: auto;">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item d-flex justify-content-between lh-sm" style="background: #C9E3E3;">
+            <div>
+              <h6 class="my-0"><strong>Tanggal Transaksi</strong></h6>
+              <small class="text-body-secondary">Jumlah Transaksi: 5</small>
+              
+            </div>
+            <span class="text-body-secondary"><h6>23-12-2023</h6></span>
+            </li>
+            <!-- <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              
+            <h6 class="my-0"><strong>ID</strong></h6>
+              <small class="text-body-secondary" style="text-align: center;"><strong>Qty</strong></small>
+            </div>
+            
+            <span class="text-body-secondary"><strong>Subtotal</strong></span>
+
+            
+            </li> -->
+
+            <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0 mb-2">ID</h6>
+              <h6 class="my-0 mb-2">Total Qty</h6>
+              <h6 class="my-0">Total Bayar</h6>
+              
+            </div>
+            <div>
+              <span class="text-body-secondary mb-2"><h6>1</h6></span>
+              <span class="text-body-secondary mb-2"><h6>10</h6></span>
+              <span class="text-body-secondary">Rp. 20.000</h6></span>
+            </div>
+            </li>
+
+            <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0 mb-2">ID</h6>
+              <h6 class="my-0 mb-2">Total Qty</h6>
+              <h6 class="my-0">Total Bayar</h6>
+              
+            </div>
+            <div>
+              <span class="text-body-secondary mb-2"><h6>1</h6></span>
+              <span class="text-body-secondary mb-2"><h6>10</h6></span>
+              <span class="text-body-secondary">Rp. 20.000</h6></span>
+            </div>
+            </li>
+
+            <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0 mb-2">ID</h6>
+              <h6 class="my-0 mb-2">Total Qty</h6>
+              <h6 class="my-0">Total Bayar</h6>
+              
+            </div>
+            <div>
+              <span class="text-body-secondary mb-2"><h6>1</h6></span>
+              <span class="text-body-secondary mb-2"><h6>10</h6></span>
+              <span class="text-body-secondary">Rp. 20.000</h6></span>
+            </div>
+            </li>
+
+
+            <li class="list-group-item d-flex justify-content-between lh-sm" style="background: #DCE4EE;">
+            <div>
+              <h6 class="my-0 mb-2"><strong>Total Barang Terjual</strong></h6>
+              <h6 class="my-0"><strong>Total Penjualan</strong></h6>
+              
+            </div>
+            <div>
+              <span class="text-body-secondary mb-2"><strong><h6>100</strong></h6></span>
+              <span class="text-body-secondary"><strong>Rp. 150.000</h6></strong></span>
+            </div>
+            
+            
+            </li>
+          </ul>
+        </div>
+        <!-- </div> -->
+
+        
+  </div>
+</div>
+</div>
+</div>
+  
+
+
