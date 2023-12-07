f9d2317c58bb85422c61c37989b1c4c3a43e0064
WulanMPF <wulanmaulidya1604@gmail.com>
2023-12-07 19:58:18 +0700

    Create tambah_pengajuan.php

---
+++ app/views/user/pengajuan/tambah_pengajuan.php
@@ -0,0 +1,32 @@
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        
+      <div class="row">
+      <div class="modal-body">
+                    <form action="<?= BASEURL?>/Pengajuan_User/prosesTambah" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="nama_produk">Nama Produk:</label>
+                <input type="text" name="nama_produk" id="nama_produk" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="harga">Harga:</label>
+                <input type="text" name="harga" id="harga" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="gambar_produk">Gambar Produk:</label>
+                <input type="file" name="gambar_produk" id="gambar_produk" class="form-control" required>
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary" style="font-size: 15px; padding: 8px 10px;">Ajukan</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+    <!-- /.content -->
+        </div>
+  
+
+
+
