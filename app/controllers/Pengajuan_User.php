af4cb38e3c7f8beb4bf4bbc241df93628fcf3d25
cindyvira03 <cindyvira929@gmail.com>
2023-12-10 18:35:47 +0700

    Update Pengajuan_User.php

--- app/controllers/Pengajuan_User.php
+++ app/controllers/Pengajuan_User.php
@@ -35,22 +35,6 @@ class Pengajuan_User extends Controller
             }
 
             // Memindahkan file yang diunggah ke direktori tujuan
-<<<<<<< HEAD
-            move_uploaded_file($tmp_gambar_produk, $dir . $gambar_produk);
-    
-            $data = [
-                'id_kategori' => isset($_POST['id_kategori']) ? $_POST['id_kategori'] : null,
-                'nama_produk' => $_POST['nama_produk'],
-                'harga' => $_POST['harga'],
-                'gambar_produk' => $gambar_produk,
-                'status_pengajuan' => isset($_POST['status']) ? $_POST['status'] : null,
-            ];
-    
-            if ($this->model('Pengajuan_Model')->add($data) > 0) {
-                Flasher::setFlash('berhasil', 'ditambahkan', 'success');
-                header('Location: ' . BASEURL . '/Pengajuan_User'); // Ganti dengan alamat tujuan setelah berhasil menambahkan data
-                exit;
-=======
             if (move_uploaded_file($tmp_gambar_produk, $dir . $gambar_produk)) {
                 $data = [
                     'id_pengajuan' => isset($_POST['id_pengajuan']) ? $_POST['id_pengajuan'] : null,
@@ -67,7 +51,6 @@ class Pengajuan_User extends Controller
                 } else {
                     Flasher::setFlash('gagal', 'ditambahkan', 'danger');
                 }
->>>>>>> 22cf0d2a11f813f3eca8bd426410ab6b819b5a34
             } else {
                 Flasher::setFlash('gagal', 'gambar gagal diunggah', 'danger');
             }
@@ -92,42 +75,6 @@ class Pengajuan_User extends Controller
 
     public function prosesUbah()
     {
-<<<<<<< HEAD
-        $pengajuanModel = $this->model('Pengajuan_Model');
-        $dataPengajuan = $pengajuanModel->getPengajuanById($_POST['id_pengajuan']); // Mendapatkan data produk yang ingin diubah
-    
-        // Jika data produk ditemukan
-        if ($dataPengajuan) {
-            // Ambil nama gambar sebelumnya dari input hidden
-            $gambar_produk_sebelumnya = $_POST['gambar_produk_sebelumnya'];
-    
-            $data = [
-                'id_pengajuan' => $_POST['id_pengajuan'],
-                'id_kategori' => $_POST['id_kategori'],
-                'nama_produk' => $_POST['nama_produk'],
-                'harga' => $_POST['harga'],
-                'gambar_produk' => $gambar_produk_sebelumnya // Menyimpan nama gambar produk yang sudah ada sebagai default
-            ];
-    
-            // Jika ada gambar yang dipilih untuk diunggah
-            if (!empty($_FILES['gambar_produk']['name'])) {
-                $gambar_produk = $_FILES['gambar_produk']['name'];
-                $tmp_gambar_produk = $_FILES['gambar_produk']['tmp_name'];
-                $dir = BASEURL . '/img/pengajuan/';
-    
-                // Memindahkan file yang diunggah ke direktori tujuan
-                move_uploaded_file($tmp_gambar_produk, $dir . $gambar_produk);
-    
-                $data['gambar_produk'] = $gambar_produk; // Mengupdate nama gambar jika ada perubahan gambar
-            }
-    
-            // Proses pembaruan data menggunakan $data
-            if ($pengajuanModel->update($data) > 0) {
-                Flasher::setFlash('berhasil', 'diperbarui', 'success');
-                header('Location: ' . BASEURL . '/Pengajuan_User'); // Ganti dengan alamat tujuan setelah berhasil memperbarui data
-                exit;
-            }
-=======
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $id_pengajuan = $_POST['id_pengajuan'];
 
@@ -151,7 +98,6 @@ class Pengajuan_User extends Controller
             Flasher::setFlash('gagal', 'Metode request tidak valid', 'danger');
             header('Location: ' . BASEURL . '/Pengajuan_User'); // Redirect jika metode request tidak valid
             exit;
->>>>>>> 22cf0d2a11f813f3eca8bd426410ab6b819b5a34
         }
     
         Flasher::setFlash('gagal', 'diperbarui', 'danger');
