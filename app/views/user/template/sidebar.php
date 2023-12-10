e15779f278e4887c1b54709b841e72a088245a87
cindyvira03 <cindyvira929@gmail.com>
2023-12-10 18:30:36 +0700

    Update sidebar.php

--- app/views/user/template/sidebar.php
+++ app/views/user/template/sidebar.php
@@ -1,13 +1,13 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-light-info elevation-4"  style="position: fixed;">
+<aside class="main-sidebar sidebar-light-info elevation-4" style="position: fixed;">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="<?= BASEURL?>/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">User Kantin</span>
+      <span style="color: white; margin-left: 15px;"><strong>User Kantin</strong></span>
     </a>
     <div class="user-panel ml-4 mt-3 pb-3 mb-3 d-flex">
-      <a class="navbar-brand ml-3" href="#" style="color: black;">
-        <i class="nav-icon fas fa-user"></i>
+      <a class="navbar-brand ml-3" style="color: white">
+        <i class="nav-icon fas fa-user mr-3"></i>
                 Nama Pegawai
             </a>
     
@@ -22,36 +22,36 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item mb-3">
-            <a href="<?= BASEURL?>/Stok_User" class="nav-link active">
-              <i class="nav-icon fas fa-box"></i>
-              <p>
+            <a href="<?= BASEURL?>/Stok_User" class="nav-link active" id="menusidebar">
+              <i class="nav-icon fas fa-box" id="iconsidebar"></i>
+              <p class="namamenu">
                 Stok
                 <!-- <i class="right fas fa-angle-left"></i> -->
               </p>
             </a>
           </li>
           <li class="nav-item mb-3">
-            <a href="<?= BASEURL ?>/Pengajuan_User" class="nav-link active">
-              <i class="nav-icon fas fa-user-tie"></i>
-              <p>
+            <a href="<?= BASEURL?>/Pengajuan_User" class="nav-link active" id="menusidebar">
+              <i class="nav-icon fas fa-envelope" id="iconsidebar" ></i>
+              <p class="namamenu">
                 Pengajuan
                 <!-- <i class="right fas fa-angle-left"></i> -->
               </p>
             </a>
           </li>
           <li class="nav-item mb-3">
-            <a href="<?= BASEURL?>/History_User" class="nav-link active">
-              <i class="nav-icon fas fa-file-alt"></i>
-              <p>
+            <a href="<?= BASEURL?>/History_User" class="nav-link active" id="menusidebar">
+              <i class="nav-icon fas fa-file-alt" id="iconsidebar"></i>
+              <p class="namamenu">
                 History Penjualan
                 <!-- <i class="right fas fa-angle-left"></i> -->
               </p>
             </a>
           </li>
           <li class="nav-item mb-3">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
-              <p>
+            <a href="#" class="nav-link active" id="menusidebar">
+              <i class="nav-icon fas fa-sign-out-alt" id="iconsidebar"></i>
+              <p class="namamenu">
                 Log Out
                 <!-- <i class="right fas fa-angle-left"></i> -->
               </p>
