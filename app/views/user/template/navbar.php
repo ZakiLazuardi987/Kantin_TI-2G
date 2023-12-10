9ddb84879ab200b95ad55e7ae65384f68d5d5da2
cindyvira03 <cindyvira929@gmail.com>
2023-12-10 18:30:31 +0700

    Update navbar.php

--- app/views/user/template/navbar.php
+++ app/views/user/template/navbar.php
@@ -1,14 +1,14 @@
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand" style="background-color: #e3f2fd;">
+  <nav class="main-header navbar navbar-expand" style="background-color: #1A2A46;">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars" style="color: #3D72AA"></i></a>
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars" style="color: #F9CC41"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-      <a class="navbar-brand" href="#" style="color: #3D72AA">
+      <a class="navbar-brand" href="#" style="color: #F9CC41">
         <img src="<?= BASEURL?>/assets/brand/polinema.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
             Kantin JTI Polinema
           </a>
@@ -19,7 +19,7 @@
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
       <li class="nav-item">
-        <a class="navbar-brand" href="#" style="color: #3D72AA">
+        <a class="navbar-brand" href="#" style="color: white">
         <i class="nav-icon fas fa-user"></i>
                 Hello User!
             </a>
