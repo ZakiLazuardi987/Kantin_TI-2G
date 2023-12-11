c3fae1304820014701f98f4fe50372f0580a1bba
cindyvira03 <cindyvira929@gmail.com>
2023-12-11 11:49:53 +0700

    Update index.php

--- app/views/user/stok/index.php
+++ app/views/user/stok/index.php
@@ -7,15 +7,18 @@
                 <h1 class="m-0">Stok Produk</h1>
             </div><!-- /.col -->
             <div class="col-sm-6 d-flex justify-content-end align-items-center">
-                <div class="dropdown mr-4">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" style="padding: 5px 7px; font-size: 12px; background: #F9CC41; color: black;">
-                        Kategori
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Semua</a></li>
-                        <li><a class="dropdown-item" href="#">Makanan</a></li>
-                        <li><a class="dropdown-item" href="#">Minuman</a></li>
-                    </ul>
+            <div class="dropdown mr-4">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" style="padding: 5px 7px; font-size: 12px; background: #F9CC41; color: black;">
+                                    Kategori
+                                </button>
+                                <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" onclick="getkategori('semua', 'Semua')">Semua</a></li>
+                                <?php
+                                foreach($data['kategori'] as $kategori){ ?>
+                                    
+                                    <li><a class="dropdown-item" onclick="getkategori('<?= $kategori['id_kategori']; ?>', '<?= $kategori['nama_kategori']; ?>')"><?php echo $kategori['nama_kategori']; ?></a></li>
+                                <?php } ?>
+                                </ul>
                 </div>
                 <form class="d-flex" role="search">
                   <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -36,7 +39,7 @@
     <div class="content">
       <div class="container-fluid">
         
-        <div class="row">
+        <div class="row" id="produkContainer">
           <?php
           foreach($data['data'] as $produk) { ?>
           <div class="card card-spacing">
@@ -90,6 +93,42 @@ function kurangistok(x) {
   });
 }
 
+// Memperbarui teks pada tombol dropdown saat item dipilih
+function setDropdownText(text) {
+    $('.dropdown-toggle').html(text);
+}
 
+  function getkategori(x, nama_kategori){
+    setDropdownText(nama_kategori);
+        let url = '<?=BASEURL?>/Produk_Admin/filterKategori/' + x;
+        $.post(url, function(data, success){
+          var produk = JSON.parse(data);
+
+        // Mengosongkan container produk
+        $('#produkContainer').empty();
+
+        // Memproses setiap produk dalam data JSON dan menambahkannya ke dalam container
+        produk.forEach(function(item) {
+            var card = `
+            <div class="card card-spacing">
+            <img src="<?= BASEURL?>/img/produk/${item.gambar_produk}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title" style="text-transform: uppercase; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><strong>${item.nama_produk}</strong></h5>
+                <p class="card-text" style="margin-bottom: 5px;">Rp. ${item.harga}</p>
+                <div class="d-flex justify-content-between lh-sm">
+                    <p class="card-text">Stok: ${item.stok}</p>
+                    <div>
+                        <button type="button" class="btn btn-primary" style="background: #1A2A46;" data-toggle="modal" data-target="#exampleModal" onclick="tambahstok('${item.id_produk}')"><i class="fa fa-plus"></i></button>
+                        <button type="button" class="btn btn-secondary" style="padding: 5px 7px; font-size: 12px; margin-top: 15px;" data-toggle="modal" data-target="#exampleModal" onclick="kurangistok('${item.id_produk}')"><i class="fa fa-minus"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+            `;
+            // Menambahkan card produk ke dalam container
+            $('#produkContainer').append(card);
+        });
+    });
+  }
   
 </script>
