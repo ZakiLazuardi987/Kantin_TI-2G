41741717231272ca406037cf5d5fccb9af531fba
cindyvira03 <cindyvira929@gmail.com>
2023-12-11 08:30:36 +0700

    Update index.php

--- app/views/user/stok/index.php
+++ app/views/user/stok/index.php
@@ -7,8 +7,8 @@
                 <h1 class="m-0">Stok Produk</h1>
             </div><!-- /.col -->
             <div class="col-sm-6 d-flex justify-content-end align-items-center">
-                <div class="dropdown ml-4">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" style="padding: 5px 7px; font-size: 12px; background: #3D72AA">
+                <div class="dropdown mr-4">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" style="padding: 5px 7px; font-size: 12px; background: #F9CC41; color: black;">
                         Kategori
                     </button>
                     <ul class="dropdown-menu">
@@ -17,6 +17,10 @@
                         <li><a class="dropdown-item" href="#">Minuman</a></li>
                     </ul>
                 </div>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-primary" type="submit">Search</button>
+                </form>
                 <ol class="breadcrumb ml-4 mr-3">
                     <li class="breadcrumb-item active"><a href="<?= BASEURL?>/Home_User">Home</a></li>
                 </ol>
@@ -44,8 +48,8 @@
                   <p class="card-text">Stok: <?php echo $produk['stok']; ?></p>
                   <div>
                   <!-- <a href="<?= BASEURL?>/Produk_Admin/formUbah/<?php echo $produk['id_produk']; ?>" class="btn btn-primary"><i class="fa fa-edit"></i>Edit</a> -->
-                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onclick="tambahstok('<?= $produk['id_produk']; ?>')"><i class="fa fa-plus"></i></button>
-                  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" onclick="kurangistok('<?= $produk['id_produk']; ?>')"><i class="fa fa-minus"></i></button>
+                  <button type="button" class="btn btn-primary" style="background: #1A2A46;" data-toggle="modal" data-target="#exampleModal" onclick="tambahstok('<?= $produk['id_produk']; ?>')"><i class="fa fa-plus"></i></button>
+                  <button type="button" class="btn btn-secondary" style="padding: 5px 7px; font-size: 12px; margin-top: 15px;" data-toggle="modal" data-target="#exampleModal" onclick="kurangistok('<?= $produk['id_produk']; ?>')"><i class="fa fa-minus"></i></button>
                     <!-- <a href="Produk_Admin/prosesHapus?action=delete&id_produk=<?php echo $produk['id_produk']; ?>" class="btn btn-danger"><i class="fa fa-trash"></i>Hapus</a> -->
 
                   </div>
