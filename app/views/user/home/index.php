4b65312f98985deba263d62abe443386399b1d82
cindyvira03 <cindyvira929@gmail.com>
2023-12-12 09:13:32 +0700

    Update index.php

--- app/views/user/home/index.php
+++ app/views/user/home/index.php
@@ -6,25 +6,7 @@
             <div class="col-sm-6">
                 <h1 class="m-0">Home</h1>
             </div><!-- /.col -->
-            <div class="col-sm-6 d-flex justify-content-end align-items-center">
-            <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-primary" type="submit">Search</button>
-      </form>
-                <div class="dropdown ml-4">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" style="padding: 5px 7px; font-size: 12px; background: #F9CC41; color: black;">
-                        Kategori
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Semua</a></li>
-                        <li><a class="dropdown-item" href="#">Makanan</a></li>
-                        <li><a class="dropdown-item" href="#">Minuman</a></li>
-                    </ul>
-                </div>
-                <ol class="breadcrumb ml-4 mr-3">
-                    <li><button type="button" class="btn" style="padding: 4px 4px; font-size: 12px; background: #F6E8C1; color: black;" data-toggle="modal" data-target="#exampleModal" onclick="transaksi()"><strong>Checkout</strong><i class="fa fa-cart-plus"></i></button></li>
-                </ol>
-            </div><!-- /.col -->
+            
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
@@ -35,43 +17,157 @@
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-2">
-                <input class="form-control me-2" type="date" id="tanggal" placeholder="Tanggal" aria-label="Tanggal">
-            </div>
-        </div>
-        <div class="row mb-2">
-           
-        <table class="table table-bordered">
-                    <thead style="background: #F6E8C1;">
+        <div class="row mb-3" style="background: #F6E8C1; border-radius: 10px; box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);">
+            <div class="col-lg-4 mt-2 mb-2">
+                <div class="box box-widget">
+                    <div class="box-body">
+                        <table width="100%">
                         <tr>
-                        <th>Pilih</th>
-                        <th>Gambar</th>
-                        <th>Nama</th>
-                        <th>Harga</th>
-                        <th>Stok</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    foreach($data['data'] as $row){
-                        echo "<tr>";
-                        echo "<td><input type='checkbox' name='id_produk'></td>";
-                        echo "<td><img style='width: 50px;' src='" . BASEURL . "/img/produk/" . $row['gambar_produk'] . "'></td>";
-                        echo "<td>" . $row['nama_produk'] . "</td>";
-                        echo "<td>" . $row['harga'] . "</td>";
-                        echo "<td>" . $row['stok'] . "</td>";
-                        echo "</tr>";
-                    }
-                    ?>
-                    </tbody>
-                </table>
-            
+                                
+                                <td style="vertical-align: top;">
+                                <label for="barcode">Tanggal</label>
+                                </td>
+                                <td>
+                                    <div class="form-group input-group">
+                                    <input type="date" id="tanggal" class="form-control">
+
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                
+                                <td style="vertical-align: top;">
+                                <label for="barcode">Produk</label>
+                                </td>
+                                <td>
+                                    <div class="form-group input-group">
+                                    <select class="form-control">
+                                        <option selected="selected">orange</option>
+                                        <option>white</option>
+                                        <option>purple</option>
+                                        </select>
+                                        <!-- <input type="hidden" id="id_produk">
+                                        <input type="text" id="barcode" class="form-control" autofocus>
+                                        <span class="input-group-btn">
+                                            <button type="button" class="btn btn-info btn-flat" data-toggle="modal" data-target="#modal-item">
+                                                <i class="fa fa-search"></i>
+
+                                            </button> -->
+
+                                        </span>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                
+                                <td style="vertical-align: top;">
+                                <label for="qty">Qty</label>
+                                </td>
+                                <td>
+                                    <div class="form-group">
+                                        <input type="number" id="qty" value="1" min="1" class="form-control">
+
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <button type="button" id="add-cart" class="btn" style="padding: 5px 7px; font-size: 12px; background: #1A2A46; color: white">
+                                        <i class="fa fa-plus"> Tambah</i>
+
+                                    </button>
+                                    <button type="button" id="add-cart" class="btn" style="padding: 5px 7px; font-size: 12px; background: #F9CC41">
+                                        <i class="fa fa-credit-card"> Bayar</i>
+
+                                    </button>
+                                </td>
+                            
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-8 mt-2 mb-2">
+                <div class="box box-widget">
+                    <div class="box-body">
+                        <div align="right">
+                            <h4><strong>Total Pembayaran</strong></h4>
+                            <h3><span id="total" style="font-size: 40pt; color: #1A2A46;">Rp 10.000</span></h3>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-lg-12 mt-3 mb-2">
+                    <div class="box box-widget">
+                        <div class="box-body table-responsive">
+                            <table class="table table-bordered">
+                                <thead style="background: #333f57">
+                                    <tr>
+                                        <th id="tabel-trans">Nama Produk</th>
+                                        <th id="tabel-trans">Qty</th>
+                                        <th id="tabel-trans">Harga</th>
+                                        <th id="tabel-trans">Subtotal</th>
+                                        <th id="tabel-trans">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="cart-table" style="background: white">
+                                    <tr>
+                                        <!-- <td colspan="9" class="text-center">Tidak ada item</td> -->
+                                        <td>risol</td>
+                                        <td>2</td>
+                                        <td>3000</td>
+                                        <td>6000</td>
+                                        <td>
+                                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" onclick="detail()" style="background: #1A2A46; margin: auto; padding: 5px 6px; font-size: 12px;">
+                                            Hapus
+                                        </button>
+                                        </td>
+                                    </tr>
+
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+        
+        <!-- <div class ="row mt-3" style="background: #F6E8C1; border-radius: 10px; box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);">
+                <div class="col-lg-12 mt-2 mb-2">
+                    <div class="box box-widget">
+                        <div class="box-body table-responsive">
+                            <table class="table table-bordered table-striped">
+                                <thead style="background: #333f57">
+                                    <tr>
+                                        <th id="tabel-trans">Nama Produk</th>
+                                        <th id="tabel-trans">Qty</th>
+                                        <th id="tabel-trans">Harga</th>
+                                        <th id="tabel-trans">Subtotal</th>
+                                        <th id="tabel-trans">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="cart-table" style="background: white">
+                                    <tr>
+                                        <td colspan="9" class="text-center">Tidak ada item</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+
         </div>
         
       </div>
     <!-- /.content -->
-    </div>
 </div>
   <!-- /.content-wrapper -->
 <!-- Modal -->
