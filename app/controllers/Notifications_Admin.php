04981375cffcbf645112f30bba73f58288714d54
Zaki Lazuardi <zakilfp987@gmail.com>
2023-12-12 08:21:13 +0700

    Update Notifications_Admin.php

--- app/controllers/Notifications_Admin.php
+++ app/controllers/Notifications_Admin.php
@@ -15,11 +15,32 @@ class Notifications_Admin extends Controller {
 
     public function notifications()
     {
-        //$data['title'] = 'Home Admin';
-        //$data['data'] = $this->model('Produk_Model')->getAllProducts();
+        $this->view('admin/notifications/updateStatus');   
+    }
 
-        $this->view('admin/notifications/detail_notif');
-        
+    public function tambah() {
+        // Logika untuk menangani permintaan tambah produk
+        $produkModel = $this->model('Produk_Model');
+
+        // Ambil data yang diperlukan dari POST atau sumber data lainnya
+        $data = [
+            'id_kategori' => $_POST['id_kategori'],
+            'nama_produk' => $_POST['nama_produk'],
+            'harga' => $_POST['harga'],
+            'gambar_produk' => $_POST['gambar_produk'],
+        ];
+
+        // Panggil method add di Produk_Model
+        $result = $produkModel->add($data);
+
+        // Handle hasil penambahan (tampilkan pesan, redirect, atau kirim respons JSON, dll.)
+        if ($result > 0) {
+            // Produk berhasil ditambahkan
+            echo "Produk berhasil ditambahkan!";
+        } else {
+            // Produk gagal ditambahkan
+            echo "Gagal menambahkan produk!";
+        }
     }
 
 }
