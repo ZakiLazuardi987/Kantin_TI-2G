04dd2b13034b3948ee21c4f231a051dbe5a5c5d5
cindyvira03 <cindyvira929@gmail.com>
2023-12-14 08:33:40 +0700

    Update index.php

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -102,9 +102,15 @@
 </style>
 
 <body>
-
+    
     <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4">
+              <?php Flasher::flash(); ?>
+            </div>
+        </div>
         <div class="row justify-content-center mt-5">
+        
             <div class="col-md-4">
                 <div class="card rounded">
                     <div class="bg">
