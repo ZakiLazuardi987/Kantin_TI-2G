58db10db95a43fd98283da6285da5129c892789f
WulanMPF <wulanmaulidya1604@gmail.com>
2023-12-17 20:25:05 +0700

    Update History_User.php

--- app/controllers/History_User.php
+++ app/controllers/History_User.php
@@ -3,17 +3,22 @@
 class History_User extends Controller {
     public function index()
     {
-        //$data['title'] = 'Home Stok';
-        //$data['data'] = $this->model('Produk_Model')->getAllProducts();
-        $data['data'] = $this->model('Transaksi_Model')->getAllTransaction();
         $data['nama_user'] = $_SESSION['username'] ?? '';
-        
+
+        // Check if a date filter is provided
+        if (isset($_POST['filter'])) {
+            $tanggalFilter = $_POST['tanggal'];
+            $data['data'] = $this->model('Transaksi_Model')->getHistoryByDate($tanggalFilter);
+        } else {
+            // If no filter, get all transactions
+            $data['data'] = $this->model('Transaksi_Model')->getAllTransaction();
+        }
+
         $this->view('user/template/header');
         $this->view('user/template/navbar', $data);
         $this->view('user/template/sidebar', $data);
         $this->view('user/historyPenjualan/index', $data);
         $this->view('admin/template/footer');
-        
     }
 
     public function detail(){
@@ -22,7 +27,6 @@ class History_User extends Controller {
 
     
     $this->view('user/historyPenjualan/detail_history');
-    
     }
     
     
