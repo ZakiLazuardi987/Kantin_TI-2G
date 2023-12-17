4a83e4bb147a419c0cbc1a0fe651fbd98ff6da93
WulanMPF <wulanmaulidya1604@gmail.com>
2023-12-17 20:01:09 +0700

    Update Laporan_Admin.php

--- app/controllers/Laporan_Admin.php
+++ app/controllers/Laporan_Admin.php
@@ -3,14 +3,17 @@
 class Laporan_Admin extends Controller {
     public function index()
     {
+        $tanggalFilter = isset($_POST['tanggalFilter']) ? $_POST['tanggalFilter'] : null;
+    $data['laporanPenjualan'] = $this->model('Transaksi_Model')->getDataByDate($tanggalFilter);
+
         //$data['title'] = 'Home Stok';
-        //$data['data'] = $this->model('Produk_Model')->getAllProducts();
+        // $data['data'] = $this->model('Transaksi_Model')->getAllTransaction();
         $data['nama_user'] = $_SESSION['username'] ?? '';
 
         $this->view('admin/template/header');
         $this->view('admin/template/navbar', $data);
         $this->view('admin/template/sidebar', $data);
-        $this->view('admin/laporanPenjualan/index');
+        $this->view('admin/laporanPenjualan/index',$data);
         $this->view('admin/template/footer');
         
     }
