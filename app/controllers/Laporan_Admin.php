8baa3bc88776da0c51780f37c8e316ba7d942816
WulanMPF <wulanmaulidya1604@gmail.com>
2023-12-17 20:56:28 +0700

    Update Laporan_Admin.php

--- app/controllers/Laporan_Admin.php
+++ app/controllers/Laporan_Admin.php
@@ -3,11 +3,6 @@
 class Laporan_Admin extends Controller {
     public function index()
     {
-        $tanggalFilter = isset($_POST['tanggalFilter']) ? $_POST['tanggalFilter'] : null;
-    $data['laporanPenjualan'] = $this->model('Transaksi_Model')->getDataByDate($tanggalFilter);
-
-        //$data['title'] = 'Home Stok';
-        // $data['data'] = $this->model('Transaksi_Model')->getAllTransaction();
         $data['nama_user'] = $_SESSION['username'] ?? '';
 
         $this->view('admin/template/header');
@@ -15,16 +10,23 @@ class Laporan_Admin extends Controller {
         $this->view('admin/template/sidebar', $data);
         $this->view('admin/laporanPenjualan/index',$data);
         $this->view('admin/template/footer');
+
+       // Check if a date filter is provided
+        if (isset($_POST['filter'])) {
+            $tanggalFilter = $_POST['tanggal'];
+            $data['data'] = $this->model('Transaksi_Model')->getLaporanByDate($tanggalFilter);
+        } else {
+            // If no filter, get all transactions
+            $data['data'] = $this->model('Transaksi_Model')->getAllTransaction();
+        }
+        $this->view('admin/laporanPenjualan/index', $data);
         
     }
 
     public function detail(){
         // $data['title'] = 'Tambah Produk';
     //$data['kategori'] = $this->model('Produk_Model')->getAllCategories();
-
-    
     $this->view('admin/laporanPenjualan/detail_laporan');
-    
     }
     
     
